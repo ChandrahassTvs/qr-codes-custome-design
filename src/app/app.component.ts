7b49ce5d4add010b01c80f48c4443c2de27212a6
ChandrahassTvs <chandrahasstvs@gmail.com>
2022-09-02 12:07:12 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from '@angular/core';
+import JSZip = require('jszip');
+import { saveAs } from 'file-saver';
 import { QR_DATA } from './data';
 
 @Component({
@@ -9,4 +11,21 @@ import { QR_DATA } from './data';
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
   QR_DATA = QR_DATA;
+
+  download() {
+    const zip = new JSZip();
+    // create a file
+    zip.file('hello.txt', 'Hello[p my)6cxsw2q');
+    // oops, cat on keyboard. Fixing !
+    zip.file('hello.txt', 'Hello World\n');
+
+    // create a file and a folder
+    zip.file('nested/hello.txt', 'Hello World\n');
+    // same as
+    zip.folder('nested').file('hello.txt', 'Hello World\n');
+    zip.generateAsync({ type: 'blob' }).then(function (content) {
+      // see FileSaver.js
+      saveAs(content, 'example.zip');
+    });
+  }
 }
