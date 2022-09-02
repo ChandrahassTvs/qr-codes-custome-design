52daed8545699917f6f01372851541764812a88a
ChandrahassTvs <chandrahasstvs@gmail.com>
2022-09-02 14:03:48 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,62 +16,31 @@ export class AppComponent {
   QR_DATA = QR_DATA;
   qrDataUrls = [];
 
-  // onQrCodeURL(localUrl: SafeUrl, qrCode) {
-  //   this.qrDataUrls.push({
-  //     id: qrCode.ID,
-  //     url: localUrl,
-  //   });
-  // }
-
   download() {
-    // const zip = new JSZip();
-    // console.log(this.qrDataUrls.length, this.qrDataUrls);
-    // this.qrDataUrls.forEach((data, i) => {
-    //   if (i <= 5) {
-    //     var blob = new Blob([data.url], {
-    //       type: 'image/svg+xml;charset=utf-8',
-    //     });
-    //     var reader = new FileReader();
-    //     reader.readAsDataURL(blob);
-    //     console.log(blob, reader);
-    //     zip.file(`${data.id}.svg`, reader.result);
-    //   }
-    // });
-    // zip.generateAsync({ type: 'blob' }).then(function (content) {
-    //   // see FileSaver.js
-    //   // saveAs(content, 'qr-codes.zip');
-    // });
     const zip = new JSZip();
     QR_DATA.forEach((data, index) => {
-      if (index < 1) {
-        let temp = this;
-        htmlToImage
-          .toBlob(document.getElementById(data.ID), {
-            type: 'image/svg+xml;charset=utf-8',
-          })
-          .then(function (dataUrl) {
-            console.log(dataUrl);
-            /* do something */
-            temp.qrDataUrls.push({
-              id: data.ID,
-              dataUrl,
-            });
-            saveAs(dataUrl, 'my-node.svg');
+      let temp = this;
+      htmlToImage
+        .toSvg(document.getElementById(data.ID), { filter: temp.filter })
+        .then(function (dataUrl) {
+          temp.qrDataUrls.push({
+            id: data.ID,
+            dataUrl,
           });
-      }
+          var dl = document.createElement('a');
+          document.body.appendChild(dl); // This line makes it work in Firefox.
+          dl.setAttribute('href', dataUrl);
+          dl.setAttribute('download', `${data.ID}.svg`);
+          dl.click();
+        });
     });
-    setTimeout(() => {
-      // console.log(this.qrDataUrls);
-      this.qrDataUrls.forEach((data, i) => {
-        console.log(window.btoa(data.dataUrl));
-        zip.file(`${data.id}.svg`, window.btoa(data.dataUrl), { base64: true });
-      });
+    // this.qrDataUrls.forEach((data, i) => {
+    //   zip.file(`${data.id}.svg`, data.dataUrl);
+    // });
 
-      zip.generateAsync({ type: 'blob' }).then(function (content) {
-        // see FileSaver.js
-        // saveAs(content, 'qr-codes.zip');
-      });
-    }, 100);
+    // zip.generateAsync({ type: 'blob' }).then(function (content) {
+    //   saveAs(content, 'qr-codes.zip');
+    // });
   }
   filter(node) {
     return node.tagName !== 'i';
