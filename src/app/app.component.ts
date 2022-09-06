c7232d701997620ffdf3001868da70adb5738d80
ChandrahassTvs <chandrahasstvs@gmail.com>
2022-09-06 17:30:21 +0530

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SafeUrl } from '@angular/platform-browser';
 import * as htmlToImage from 'html-to-image';
 import { toSvg } from 'html-to-image';
 import { documentToSVG, elementToSVG, inlineResources } from 'dom-to-svg';
+import { potrace } from 'potrace';
 
 @Component({
   selector: 'my-app',
@@ -20,36 +21,41 @@ export class AppComponent {
     const zip = new JSZip();
     QR_DATA.forEach(async (data, index) => {
       if (index >= start && index < end) {
-        // let temp = this;
-        // htmlToImage
-        //   .toSvg(document.getElementById(data.ID))
-        //   .then(function (dataUrl) {
-        //     temp.qrDataUrls.push({
-        //       id: data.ID,
-        //       dataUrl,
-        //     });
-        //     var dl = document.createElement('a');
-        //     document.body.appendChild(dl); // This line makes it work in Firefox.
-        //     dl.setAttribute('href', dataUrl);
-        //     dl.setAttribute('download', `${data.ID}.svg`);
-        //     dl.click();
-        //   });
+        let temp = this;
+        htmlToImage
+          .toPng(document.getElementById(data.ID))
+          .then(function (dataUrl) {
+            temp.qrDataUrls.push({
+              id: data.ID,
+              dataUrl,
+            });
+            // var dl = document.createElement('a');
+            // dl.setAttribute('href', dataUrl);
+            // dl.setAttribute('download', `${data.ID}.png`);
+            // dl.click();
 
+            potrace.trace('./path/to/image.png', function (err, svg) {
+              if (err) throw err;
+              console.log(svg);
+            });
+
+            // console.log(dataUrl.replace(/^data:image\/(png|jpg);base64,/, ''));
+            // zip.file(`${data.ID}.png`, dataUrl, { base64: true });
+          });
         // Capture specific element
-        const svgDocument = elementToSVG(document.getElementById(data.ID));
+        // const svgDocument = elementToSVG(document.getElementById(data.ID));
 
-        // Inline external resources (fonts, images, etc) as data: URIs
-        await inlineResources(svgDocument.documentElement);
+        // // Inline external resources (fonts, images, etc) as data: URIs
+        // await inlineResources(svgDocument.documentElement);
 
-        // Get SVG string
-        const svgString = new XMLSerializer().serializeToString(svgDocument);
-        // zip.file(`${data.ID}.svg`, svgString);
-        console.log(svgDocument, svgString);
+        // // Get SVG string
+        // const svgString = new XMLSerializer().serializeToString(svgDocument);
+        // // zip.file(`${data.ID}.svg`, svgString);
+        // console.log(svgDocument, svgString);
       }
     });
-
     zip.generateAsync({ type: 'blob' }).then(function (content) {
-      // saveAs(content, 'qr-codes.zip');
+      saveAs(content, 'qr-codes.zip');
     });
   }
   filter(node) {
